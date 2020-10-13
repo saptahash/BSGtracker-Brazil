f89c78e1b84f3b73993974e20bf19fc15708e106
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2020-10-13 03:44:41 +0100

    install dev version of tibble

--- sysrequirements.sh
+++ sysrequirements.sh
@@ -2,4 +2,5 @@
 sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("tidyverse", "rlang", "here", "lubridate", "janitor", "haven"))'
+Rscript -e 'remotes::install_cran(c("tidyverse", "rlang", "here", "lubridate", "janitor", "haven", "devtools"))'
+Rscript -e 'devtools::install_github("tidyverse/tibble")'
